fb7d82cc8c8df4fe393e68f44bd098538e72f875
Raulscj <radscjggdiosteama@gmail.com>
2025-05-05 18:48:00 -0400

    part 3

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import middleWares from "./middlewares";
 import router from "./routes";
 import Server from "./server";
+//import datasource from "./datasource";
+//import { User } from "./entities/user";
 
 // Configuración del puerto
 const PORT: number = Number(process.env.PORT) || 3000;
@@ -12,3 +14,19 @@ const server = new Server({
   routes: [router],
 });
 server.listen();
+
+/*
+// Conexión a la base de datos
+datasource
+  .initialize()
+  .then(async () => {
+    const user = new User();
+    user.firstName = 'Nombre';
+    user.lastName = 'Apellido';
+
+    const result = await datasource.getRepository(User).save(user);
+    console.log(result);
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization:', err);
+  });*/
